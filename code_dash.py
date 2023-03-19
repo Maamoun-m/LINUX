b02c69d58087c7d742776a785b199431e51def75
Maamoun-m <93276355+Maamoun-m@users.noreply.github.com>
2023-03-19 13:01:00 +0100

    Update code_dash.py

--- code_dash.py
+++ code_dash.py
@@ -4,26 +4,32 @@ Created on Tue Jan 31 01:25:39 2023
 
 @author: mekki
 """
-import pandas as pd
+import subprocess
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
+from dash.dependencies import Input, Output
 
-# Read data from text file into pandas DataFrame
-df = pd.read_csv('data.txt', delimiter=' ', header=None, names=['date', 'value'])
-df['date'] = pd.to_datetime(df['date'])
+# retrieve initial data
+index_value = subprocess.check_output(["curl", "https://www.marketwatch.com/investing/index/nik/charts?countryCode=JP", "|", "grep", "'<bg-quote class=\"value'>\"'", "|", "sed", "'s/<bg-quote class=\"value\">//g'", "|", "sed", "'s/<\\/bg-quote>//g'", "|", "awk", "'{print $1}'"]).decode("utf-8").strip()
 
-# Create Dash app and layout
+# create the app
 app = dash.Dash(__name__)
-app.layout = html.Div(children=[
-    html.H1(children='Nikkei Index'),
-    dcc.Graph(
-        id='time-series',
-        figure=px.line(df, x='date', y='value', title='Nikkei Index Time Series')
-    )
+
+# define the layout
+app.layout = html.Div([
+    html.H1("Nikkei Index"),
+    html.Div(id="index-value", children=f"Current value: {index_value}"),
+    dcc.Interval(id="interval-component", interval=60*1000, n_intervals=0)
 ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# define the callbacks
+@app.callback(Output("index-value", "children"),
+              Input("interval-component", "n_intervals"))
+def update_index_value(n):
+    index_value = subprocess.check_output(["curl", "https://www.marketwatch.com/investing/index/nik/charts?countryCode=JP", "|", "grep", "'<bg-quote class=\"value'>\"'", "|", "sed", "'s/<bg-quote class=\"value\">//g'", "|", "sed", "'s/<\\/bg-quote>//g'", "|", "awk", "'{print $1}'"]).decode("utf-8").strip()
+    return f"Current value: {index_value}"
 
+# run the app
+if __name__ == "__main__":
+    app.run_server(debug=True)
